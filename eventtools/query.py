7c6f1f90ba5128eacb002991389fdf931a1d207c
anthony percy <anthcp@Junes-iMac.local>
2025-12-20 16:15:34 +1030

    again

--- eventtools/query.py
+++ eventtools/query.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils import timezone as dj_timezone
 from django.core.exceptions import ValidationError
 from dateutil.rrule import rrulestr, rruleset
+from django.db.models import Q, Case, When, Value
 
 import pendulum
 from .utils import as_datetime, tzDateFactory, combine_occurrences, filter_invalid, filter_from, first_item, combine_occurrences
