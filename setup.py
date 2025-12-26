4d3a812d3fd276d8bbdb6f5a48de7d95b6c4ef4d
VK5QS - Anthony <anthcp@gmail.com>
2025-12-26 14:07:48 +1030

    updated setup

--- setup.py
+++ setup.py
@@ -1,46 +1,65 @@
 #!/usr/bin/env python
-# coding: utf8
+# coding: utf-8
 
 import os
-
 from setuptools import setup, find_packages
 
-# if there's a converted readme, use it, otherwise fall back to markdown
-if os.path.exists('README.rst'):
-    readme_path = 'README.rst'
-else:
-    readme_path = 'README.md'
+# Choose README type dynamically
+readme_path = 'README.rst' if os.path.exists('README.rst') else 'README.md'
+content_type = (
+    'text/x-rst' if readme_path.endswith('.rst') else 'text/markdown'
+)
 
-# avoid importing the module
+# Load version without importing package
 exec(open('eventtools/_version.py').read())
 
 setup(
     name='django-eventtools',
     version=__version__,
-    description='Recurring event tools for django',
+    description='Recurring event tools for Django',
     long_description=open(readme_path).read(),
-    author='Greg Brown',
-    author_email='greg@gregbrown.co.nz',
-    url='https://github.com/gregplaysguitar/django-eventtools',
-    packages=find_packages(exclude=('tests', )),
+    long_description_content_type=content_type,
+    author='Anthony Percy',
+    author_email='anthony.percy@systemscom.info',
+    url='https://github.com/anthcp/django-eventtools',
+    packages=find_packages(exclude=('tests',)),
     license='BSD License',
     zip_safe=False,
-    platforms='any',
-    install_requires=['Django>=1.8', 'python-dateutil>=2.1', 'six>=1.14.0'],
+
+    # 🚀 Modern Python versions only
+    python_requires='>=3.9',
+
+    install_requires=[
+        'Django>=3.2,<5.2',
+        'python-dateutil>=2.8.2',
+    ],
+
     include_package_data=True,
     package_data={},
+
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
+
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+
         'Framework :: Django',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
+        'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.0',
+        'Framework :: Django :: 5.1',
+
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
 )
